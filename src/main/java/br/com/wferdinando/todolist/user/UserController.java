4a04ba298d4c3b64b97cf096f0151a997effa800
Willyan Trautwein Ferdinando <willyantferdinando@gmail.com>
2023-10-10 20:21:55 -0300

    Validação UserMOdel

--- src/main/java/br/com/wferdinando/todolist/user/UserController.java
+++ src/main/java/br/com/wferdinando/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.wferdinando.todolist.user;
 
+import org.apache.catalina.connector.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +18,14 @@ public class UserController {
     private final IUserRepository userRepository;
 
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel) {
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUserName(userModel.getUserName());
+
+        if(user != null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+        
         var userCreated = this.userRepository.save(userModel);
-        return userCreated;
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
--- src/main/java/br/com/wferdinando/todolist/user/UserModel.java
+++ src/main/java/br/com/wferdinando/todolist/user/UserModel.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -23,8 +24,12 @@ public class UserModel {
     @Id
     @GeneratedValue(generator = "UUID")
     private UUID id;
+
+    @Column(unique = true)
     private String userName;
+    
     private String name;
+    
     private String password;
 
     @CreationTimestamp
