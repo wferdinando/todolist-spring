8af62b9af1befdb26ef374064aa8fbd9dfab48f2
Willyan Trautwein Ferdinando <willyantferdinando@gmail.com>
2023-10-10 20:07:31 -0300

    IUserRepository

--- src/main/java/br/com/wferdinando/todolist/user/UserController.java
+++ src/main/java/br/com/wferdinando/todolist/user/UserController.java
@@ -5,12 +5,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
-    
+
+    private final IUserRepository userRepository;
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel){
-        System.out.println(userModel.getName());
+    public UserModel create(@RequestBody UserModel userModel) {
+        var userCreated = this.userRepository.save(userModel);
+        return userCreated;
     }
 }
